f1643cf785601a41b42b78b50c5affc35ed7c177
mariaFlorenciaMerzario <maria.merzario@davinci.edu.ar>
2023-08-23 11:59:49 -0300

    cart not found

--- src/Components/Container/ItemCount/ItemCount.jsx
+++ src/Components/Container/ItemCount/ItemCount.jsx
@@ -1,28 +1,28 @@
 import Button from "../button/Button"
 import { useState } from "react"
-const ItemCount = (stock, initial, onAdd) => {
+const ItemCount = (props) => {
   
-    const {quantity, setQuantity} = useState(initial)
+    const [quantity, setQuantity] = useState(props.initial)
     const increment =()=>{
         alert('hola increment')
-        if(quantity< stock){
-            setQuantity(quantity+1)
+        if(quantity< props.stock){
+            setQuantity(props.quantity+1)
         }
     }
     const decrement =()=>{
-        if(quantity< stock){
+        if(quantity< props.stock){
             alert('decrement decrement')
-            setQuantity(quantity-1)
+            setQuantity(props.quantity-1)
         }
     }
     return(
        <>
         <div>
-            <Button class='btn btn-light' id="btnMinQuantity" value='-1' onClick={decrement}/>
+            <Button class='btn btn-light' id="btnMinQuantity" value='- 1' onClick={decrement}/>
             <span>{quantity}</span>
-            <Button class='btn btn-light' id="btnAddQuantity" value='+1' onClick={increment}/>
+            <Button class='btn btn-light' id="btnAddQuantity" value='+ 1' onClick={increment}/>
         </div>
-            <Button onClick={()=> onAdd=(quantity)} disabled={!stock} class='btn btn-warning m-2' value='Agregar al Carrito'/>
+            <Button onClick={()=> props.onAdd(quantity)} disabled={!props.stock} class='btn btn-warning m-2' value='Agregar al Carrito'/>
        </>
     )
 }
