de651ee425e5b7a17ba6c814494bb464211dde65
mariaFlorenciaMerzario <maria.merzario@davinci.edu.ar>
2023-08-30 14:04:19 -0300

    add tooltip and modal windows

--- src/Components/Container/Card.jsx
+++ src/Components/Container/Card.jsx
@@ -1,21 +1,98 @@
 import './Card.css';
+import React from "react";
 import ItemCount from './ItemCount/ItemCount';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import Modal from 'react-modal';
+import ReactDOM from 'react-dom';
+
+import 'react-tooltip/dist/react-tooltip.css'
+import { Tooltip } from 'react-tooltip'
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement(document.querySelector('.card'));
+
 
 const Card = (props) => {
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
     return(
-      
       <div className="card m-1"style={{width: '300px'}}>
-        <div className="card-body">
-       
+        <div className="card-body " id='yourAppElement'>
         <img src={require('../Assets/'+props.image)} style={{width: '150px'}}alt={props.name}/>
+      
+       <button 
+          
+           onClick={openModal}
+            data-tooltip-id="my-tooltip" 
+            data-tooltip-content="Ver más" 
+            className='verMas'>
+              <FontAwesomeIcon className="mx-2" icon={faMagnifyingGlass} />
+       </button>
+
+      
+      <Tooltip 
+        id="my-tooltip" 
+        place="right"
+        type="info"
+        effect="float"
+      />
+
+       {/* <Tooltip data-tooltip-id="my-tooltip" data-tooltip-content="Hello mundo!">
+        este es el mensaje mio
+       </Tooltip> */}
             <p className="h4">{props.name} </p > 
             <p>${props.price}.-</p> 
-            <p>{props.description}</p>
            <div className='d-flex justify-content-center'>
       </div>
-          <ItemCount initial={0} stock={10} onAdd={(quantity) =>console.log('Cantidad agregada' , quantity)}/>  
-          
+          <ItemCount initial={0} stock={10} onAdd={(quantity) =>console.log('Cantidad agregada' , quantity)}/>    
         </div>
+        <Modal
+          isOpen={modalIsOpen}
+        //  onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+          ariaHideApp={false}
+        >
+        <div>
+              <div className='d-flex'>
+                <img className="mx-4 vertical-align-center" src={require('../Assets/'+props.image)} style={{width: '150px'}}alt={props.name}/>
+                <h2>{props.name}</h2>
+              </div>
+              <div className='m-4'>
+                <p>{props.description}</p>
+                <p>Precio: {props.price}</p>
+                <ItemCount/><button onClick={closeModal} className='btn btn-dark'>Volver atrás</button>
+             </div>
+        </div>
+       
+      </Modal>
       </div>
    
     )
--- src/Components/Container/Main.jsx
+++ src/Components/Container/Main.jsx
@@ -1,19 +1,74 @@
 import Card from "./Card"
+let products =[
+    {
+        image: 'argentinas.jpg',
+        name: 'Empanada de Carne',
+        price: 500,
+        description:'Un producto elaborado a base de Carne, huevo, cebolla y aceitunas'
+    },
+    {
+        image: 'capresse.jpg',
+        name: 'Empanada Caprese',
+        price: 500,
+        description:'Un producto elaborado a base de Queso, tomate y albahaca'
+    },
+    {
+        image: 'jamonQueso.jpg',
+        name: 'Empanada de Jamón y Queso',
+        price: 500,
+        description:'Un producto elaborado a base de Jamón y Queso'
+    },
+    {
+        image: 'criolla.jpg',
+        name: 'Empanada Criolla',
+        price: 650,
+        description:'Un producto elaborado a base de Carne, huevo, cebolla y aceitunas'
+    },
+    {
+        image: 'pollo.jpg',
+        name: 'Empanada de Pollo',
+        price: 450,
+        description:'Un producto elaborado a base de Pollo, huevo, cebolla y aceitunas'
+    },
+    {
+        image: 'saltena.jpg',
+        name: 'Empanada Salteña',
+        price: 500,
+        description:'Un producto elaborado a base de Carne, huevo, cebolla y aceitunas'
+    },
+    {
+        image: 'roqueforJamon.jfif',
+        name: 'Empanada de Roquefort y Jamón',
+        price: 700,
+        description:'Un producto elaborado a base de Roquefort y Jamón'
+    },
+    {
+        image: 'argentinas.jpg',
+        name: 'Empanada de Carne',
+        price: 500,
+        description:'Un producto elaborado a base de Carne, huevo, cebolla y aceitunas'
+    }
+    ]
 
 const Main = () => {
     return(
+      
         <>
-         <div className="m-2 row d-flex flex-md-row flex-column justify-content-center">      
-            <Card image='argentinas.jpg' name="Empanada de Carne" price="500" description="Un producto elaborado a base de Carne, huevo, cebolla y aceitunas"/>
-            <Card image='carneFrita.jpeg' name="Empanada de Carne Frita" price="500" description="Un producto elaborado a base de  Carne, huevo, cebolla y aceitunas "/>
-            <Card image='capresse.jpg' name="Empanada Capresse" price="600" description="Un producto elaborado a base de Queso, tomate y albahaca "/>
-            <Card image='jamonQueso.jpg' name="Empanada de Jamón y Queso" price="500" description="Un producto elaborado a base de Jamón y Queso"/>
-            <Card image='criolla.jpg' name="Empanada Criolla" price="500" description="Un producto elaborado a base de Carne, huevo, cebolla y aceitunas"/>
-            <Card image='pollo.jpg' name="Empanada de Pollo" price="500" description="Un producto elaborado a base de Pollo, huevo, cebolla y aceitunas "/>
-            <Card image='saltena.jpg' name="Empanada Salteña" price="500" description="Un producto elaborado a base de Carne, huevo, cebolla y aceitunas"/>
-            <Card image='roqueforJamon.jfif' name="Empanada de Roquefort y Jamón" price="650" description="Un producto elaborado a base de roquefort y jamón "/>
-         </div>
+        <div className="m-2 row d-flex flex-md-row flex-column justify-content-center">   
+
+         {products.map((element, index) => (
+          <Card
+            key={index}
+            image={element.image}
+            name={element.name}
+            price={element.price}
+            description={element.description}
+         
+            />
+         ))}         
+      </div>
         </>
+      
     )
 }
 export default Main
