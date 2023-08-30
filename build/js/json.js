3c56f28f83b754f98cd363611c4fc5da9f3d0be5
mariaFlorenciaMerzario <maria.merzario@davinci.edu.ar>
2023-08-30 14:05:59 -0300

    add build modal y tooltip

---
+++ build/js/json.js
@@ -0,0 +1,48 @@
+const products =[
+{
+    image: 'argentinas.jpg',
+    name: 'Empanada de Carne',
+    price: 500,
+    description:'Un producto elaborado a base de Carne, huevo, cebolla y aceitunas'
+},
+{
+    image: 'capresse.jpg',
+    name: 'Empanada Caprese',
+    price: 500,
+    description:'Un producto elaborado a base de Queso, tomate y albahaca'
+},
+{
+    image: 'jamonQueso.jpg',
+    name: 'Empanada de Jamón y Queso',
+    price: 500,
+    description:'Un producto elaborado a base de Jamón y Queso'
+},
+{
+    image: 'criolla.jpg',
+    name: 'Empanada Criolla',
+    price: 650,
+    description:'Un producto elaborado a base de Carne, huevo, cebolla y aceitunas'
+},
+{
+    image: 'pollo.jpg',
+    name: 'Empanada de Pollo',
+    price: 450,
+    description:'Un producto elaborado a base de Pollo, huevo, cebolla y aceitunas'
+},
+{
+    image: 'saltena.jpg',
+    name: 'Empanada Salteña',
+    price: 500,
+    description:'Un producto elaborado a base de Carne, huevo, cebolla y aceitunas'
+},
+{
+    image: 'roqueforJamon.jfif',
+    name: 'Empanada de Roquefort y Jamón',
+    price: 700,
+    description:'Un producto elaborado a base de Roquefort y Jamón'
+},
+]
+
+
+//const Pedro = array.find((item) => item.id === 2)
+ 
