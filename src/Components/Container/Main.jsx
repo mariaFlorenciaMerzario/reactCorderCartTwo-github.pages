1afeca6d0a15f06b9d2e1a063868d8571d59ed32
mariaFlorenciaMerzario <maria.merzario@davinci.edu.ar>
2023-09-15 13:40:39 -0300

    add filteredData

--- src/Components/Container/Main.jsx
+++ src/Components/Container/Main.jsx
@@ -4,22 +4,21 @@ import { useState, useEffect } from "react"
 import SpinnerB from "../Spinner/SpinnerB"
 
 import { useParams } from "react-router-dom"
-import InputSearch from "./InputSearch"
 const Main = () => {
     const [products, setProducts] = useState([])
-    const [data, setData] = useState([]);
     const [term, setTerm] = useState("");
    
-    function searchingTerm(term) {
-        let result = products.map(item =>{
-            item.name.includes(term)
-        })
-        if(result.length >0){
-            setData(data)
-        } 
-      }
+
+      const handleSearch = (event) => {
+        setTerm(event.target.value);
+        };
+
+      const filteredData = products.filter((item) =>
+        item.name.toLowerCase().includes(term.toLowerCase())
+      );
+
       useEffect(() => {
-        setData(products);
+        setProducts(products);
       }, [products]);
     
     useEffect(() =>{
@@ -59,14 +58,20 @@ const Main = () => {
             name="term" */}
             <form>
                 
-            </form>
-               <input type="text" onChange={e => setTerm(e.target.value)}/>
-          
+            <input
+           
+                type="text"
+                placeholder="Ingresá el producto buscado..."
+                value={term}
+                onChange={handleSearch}
+                className="form-control w-25 m-auto bg-light"
+            />
+          </form>
         
 
         { products.length > 0 ?
             <div className="m-2 row d-flex flex-md-row flex-column justify-content-center">
-                {products.map((element, index) => (
+                {filteredData.map((element, index) => (
                 <Card
                     id={element.id}
                     key={index}
@@ -80,27 +85,6 @@ const Main = () => {
             </div>
         :<SpinnerB/>
         }
-            { data.length > 0 ?
-            <div className="m-2 row d-flex flex-md-row flex-column justify-content-center">
-                {console.log('data')}
-                {console.log(data)}
-                 {data.map(searchingTerm(term)).map(element => ( 
-                <Card
-                    id={element.id}
-                    key={element.id}
-                    image={element.image}
-                    name={element.name}
-                    price={element.price}
-                    description={element.description}
-                    category={element.category}
-                />
-                ))}          
-            </div>
-        :<SpinnerB/>
-        }
-    
-
-      
         </>
       
     )
